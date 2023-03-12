2e77b0ae7f35136c04cde527c4c8b36cc4af0656
Vanura <Efimovv2003@yandex.ru>
2023-03-12 16:13:31 +0700

    Задание номер 2

---
+++ hwl1ex2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Writeline("Введите переменную a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Writeline("Введите переменную b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b) {int max=a;} else {int max = b;};
+ Console.Writeline(max);
