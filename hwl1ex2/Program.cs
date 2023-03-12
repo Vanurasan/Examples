ac87aab7be12fe5b3ab023813adaa6def53b4926
Vanura <Efimovv2003@yandex.ru>
2023-03-12 16:16:26 +0700

    Задание номер 2

--- hwl1ex2/Program.cs
+++ hwl1ex2/Program.cs
@@ -3,4 +3,5 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Writeline("Введите переменную b");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a>b) {int max=a; int min = b;} else {int max = b; int min = a;};
- Console.Writeline(max);
+ Console.Writeline("Максимальное:"+ max);
+ Console.Writeline("Минимальное:"+ min);
