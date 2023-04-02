5b6906e8d2d8c808db1dc0977006baf0706d650b
Vanura <Efimovv2003@yandex.ru>
2023-04-02 16:47:15 +0700

    Задание 19

---
+++ hwl3ex19/Program.cs
@@ -0,0 +1,6 @@
+﻿char[] a = Console.ReadLine().ToCharArray();
+            if ((a[0] == a[4]) && (a[1] == a[3]))
+            {
+                Console.WriteLine("Да");
+            }
+            else { Console.WriteLine("Нет"); }
